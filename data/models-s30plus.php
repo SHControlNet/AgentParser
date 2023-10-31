756676d5d75754223e72ad0658c418767e6a6a5b
My User Interface Design <32391106+MasDenk@users.noreply.github.com>
2023-10-31 20:31:34 +0700

    -

---
+++ data/models-s30plus.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AgentParser\Data;
+
+use AgentParser\Constants\Flag;
+
+DeviceModels::$S30PLUS_MODELS = [
+    '215'                                       => [ 'Nokia', '215' ],
+    '220'                                       => [ 'Nokia', '220' ],
+    '222'                                       => [ 'Nokia', '222' ],
+    '225'                                       => [ 'Nokia', '225' ],
+    '230'                                       => [ 'Nokia', '230' ]
+];
